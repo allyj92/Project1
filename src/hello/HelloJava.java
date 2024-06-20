bd76b540660c765ea8d052e9bb0edafcb31f4c8a
G203 <G203@DESKTOP-JHI7NMB>
2024-06-20 15:57:02 +0900

    첫 프로젝트 생성

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,14 @@
+package hello;
+
+/*
+ * 작성일자 : 2024-06-20
+ * 작성자 : 임은재
+ * */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("ImEunjae"); // 메세지를 출력하는 함수
+
+	}
+	// ctrl + shift + (+/-)
+}
